f7c677c8bc08e862666a68b2a1c2cc54053e947f
Gabriel Bodeen <gabebodeen@gmail.com>
2018-11-12 17:18:36 -0600

    Bind on the step method allows the tests to pass.

--- src/dancer.js
+++ src/dancer.js
@@ -3,19 +3,25 @@ const makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
 
   let obj = this;
-  this.step = function() {
-    setTimeout(obj.step, timeBetweenSteps);
-  };
-  this.step();
+  this.step = (function() {
+    // console.log(this);
+    // console.log(obj);
+    // setTimeout(obj.step, timeBetweenSteps);
+    setTimeout(this.step, timeBetweenSteps);
+  }).bind(this);
 
-  this.setPosition = function(top, left) {
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);
-  };
-
+  // console.log(this);
+  this.step(this);
   this.setPosition(top, left);
 
+};
+// makeDancer.prototype.step = function () {
+//   setTimeout(obj.step, timeBetweenSteps);
+// }
+makeDancer.prototype.setPosition = function(top, left) {
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
 };
